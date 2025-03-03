58f9c09b22c2bddce31b9590e7c33c6147a1e7e5
gtrx32 <gtrx32@bk.ru>
2025-03-03 13:04:24 +0300

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products.index', ['products' => []]);
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -19,14 +15,5 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('products', ProductController::class);
-/* 
-Route::get('/products', [ProductController::class, 'index'])->name('product.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('/products', [ProductController::class, 'store'])->name('product.store');
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('product.show');
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
-Route::patch('/products/{product}', [ProductController::class, 'update'])->name('product.update');
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
-*/
 
 require __DIR__.'/auth.php';
