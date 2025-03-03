5e6595038e332d6dca1615202af703b81f96432d
gtrx32 <gtrx32@bk.ru>
2025-03-03 13:20:04 +0300

    format

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
                 <ul>
-                    @foreach($products as $product)
+                    @foreach ($products as $product)
                         <li class="border-b py-2">
                             {{ $product->name }} - {{ $product->price }} руб.
                         </li>
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ Route::middleware('auth')->group(function () {
 
 Route::resource('products', ProductController::class);
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
