fdf538fbc5a59ab205334a5c7f9a76790e657937
gtrx32 <gtrx32@bk.ru>
2025-03-04 14:47:52 +0300

    index page changes

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,17 +1,37 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
-            {{ __('Все товары') }}
+            {{ __('Список товаров') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="bg-white shadow-lg sm:rounded-lg border border-gray-200 p-6">
-                <div class="space-y-6">
+            <div class="bg-white shadow-lg sm:rounded-lg border border-gray-200 p-8">
+                @if (session('success') || session('error'))
+                    <div class="mb-8">
+                        @if (session('success'))
+                            <div class="p-4 mb-4 text-green-800 bg-green-100 rounded-lg">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+                        @if (session('error'))
+                            <div class="p-4 mb-4 text-red-800 bg-red-100 rounded-lg">
+                                {{ session('error') }}
+                            </div>
+                        @endif
+                    </div>
+                @endif
+                <div class="mb-8">
+                    <x-primary-button-link href="{{ route('products.create') }}" class="">
+                        Добавить товар
+                    </x-primary-button-link>
+                </div>
+                <div>
                     @foreach ($products as $product)
                         <div
-                            class="flex justify-between items-center gap-6 p-4 border-b border-gray-200 hover:bg-gray-100 rounded-lg transition duration-200">
+                            class="relative flex justify-between items-center gap-6 py-4 border-b border-gray-200 hover:bg-gray-100 transition duration-200">
+                            <a class="absolute w-full h-full top-0 left-0"
+                                href="{{ route('products.show', $product->id) }}"></a>
                             <div class="w-16 h-16 flex-shrink-0">
                                 <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}"
                                     class="w-full h-full object-cover rounded-lg shadow-md">
@@ -24,17 +44,15 @@
                                 {{ number_format($product->price, 2) }} ₽
                             </div>
                             <div class="flex items-center gap-3">
-                                <a href="{{ route('products.edit', $product->id) }}"
-                                    class="border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white font-medium py-2 px-4 rounded-lg transition duration-150">
+                                <x-secondary-button-link href="{{ route('products.edit', $product) }}" class="z-10">
                                     Изменить
-                                </a>
-                                <form action="{{ route('products.destroy', $product->id) }}" method="POST">
+                                </x-secondary-button-link>
+                                <form action="{{ route('products.destroy', $product) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit"
-                                        class="border border-red-500 text-red-500 hover:bg-red-500 hover:text-white font-medium py-2 px-4 rounded-lg transition duration-150">
+                                    <x-danger-button type="submit" class="z-10 relative">
                                         Удалить
-                                    </button>
+                                    </x-danger-button>
                                 </form>
                             </div>
                         </div>
