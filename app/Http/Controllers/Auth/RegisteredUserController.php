80a496bb555ef0a1c534e187c277f7a8bb8aa486
gtrx32 <gtrx32@bk.ru>
2025-03-28 18:01:49 +0300

    добавил создание корзины при создании пользователя

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,6 +41,8 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $user->cart()->create();
+
         event(new Registered($user));
 
         Auth::login($user);
